d238e32f7c86a3a40460c2a6002301a786bd632a
MordorHD <allofheroplayer1@gmail.com>
2024-04-08 20:14:24 +0200

    added terminal that can be toggled

---
+++ i3/toggle_terminal.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+WINID=$(xdotool search --classname TogglingTerminal)
+
+NUM=wc -w <<< $WINID
+
+if [ $NUM -gt 1 ]
+then
+	FIRST=true
+	for id in $WINID
+	do
+		if $FIRST
+		then
+			FIRST=false
+			continue
+		fi
+		xdotool windowkill $id
+	done
+fi
+
+if [ -z $WINID ]
+then
+	urxvt -name TogglingTerminal &
+
+	while [ -z "$WINID" ]
+	do
+		WINID=$(xdotool search --classname TogglingTerminal)
+
+		sleep 0.1
+	done
+
+	exit
+fi
+
+xwininfo -id $WINID | grep -q "Map State: IsUnMapped"
+if [ "$?" = "0" ]
+then
+	xdotool windowmap $WINID
+else
+	xdotool windowunmap $WINID
+fi
