0e98dea464c3a5e684021f78d3567e0d6e2eee25
MordorHD <allofheroplayer1@gmail.com>
2024-04-28 21:10:31 +0200

    improved lf (but tried replacing with yazi)

--- bashrc
+++ bashrc
@@ -24,8 +24,6 @@ alias pw="pwmgr"
 
 alias vi="vim -c 'set laststatus=0'"
 
-alias lf="lf-wrapper.sh"
-
 alias ls="exa"
 
 alias alacritty='WINIT_X11_SCALE_FACTOR=1 alacritty'
@@ -33,7 +31,6 @@ alias alacritty='WINIT_X11_SCALE_FACTOR=1 alacritty'
 mkcd() {
 	mkdir "$1" && cd "$1"
 }
-
 cdb() {
 	cd ~/ext || return 1
 	if [ -n "$1" ]
@@ -48,6 +45,15 @@ cdc() {
 	then
 		cd "$1"
 	fi
+}
+
+y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
 }
 
 export CPATH="/usr/include/freetype2"
@@ -73,10 +79,29 @@ screen_cd() {
 
 alias cd=screen_cd
 
+lf_cd() {
+	cd "$(/home/steves/.config/lf/lf-wrapper.sh -print-last-dir "$@")"
+}
+
+alias lf=lf_cd
+
+alias z=zoxide
+
+eval "$(zoxide init bash)"
+
+# clipboard copy
+alias c="xclip -selection clipboard"
+
+# clipboard copy with mime type
+ci() {
+	file="$1"
+	shift
+	mime_type=$(file -Lb --mime-type -- "$file")
+	xclip -selection clipboard -t $mime_type "$file" $@
+}
+
 source /usr/share/fzf/key-bindings.bash
 source /usr/share/fzf/completion.bash
 source /usr/share/doc/pkgfile/command-not-found.bash
-
-eval "$(zoxide init bash)"
 
 [[ ${BLE_VERSION-} ]] && ble-attach
--- lf/preview.sh
+++ lf/preview.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 draw() {
 	~/.config/lf/draw_img.sh "$@"
 	exit 1
@@ -18,55 +19,60 @@ cache() {
 file="$1"
 shift
 
+mime_type="$(file -Lb --mime-type -- "$file")"
+
+case "$mime_type" in
+application/zip)
+	unzip -l "$file"
+	;;
+application/x-tar)
+	tar -tf "$file"
+	;;
+application/x-bzip2)
+	tar -tjf "$file"
+	;;
+application/gzip)
+	tar -tzf "$file"
+	;;
+application/x-rar)
+	unrar l "$file"
+	;;
+text/*)
+	highlight -O ansi -lVJ $3 "$file"
+	;;
+esac
+
 if [ -n "$FIFO_UEBERZUG" ]
 then
-	mime_type="$(file -Lb --mime-type -- "$file")"
 	case "$mime_type" in
-		application/pdf)
-			cache=$(hash "$file")
-			cache "$cache".jpg "$@"
-			pdftoppm -jpeg -f 1 -singlefile "$file" "$cache"
-			draw "$cache".jpg "$@"
-			;;
-		application/zip)
-			unzip -l "$file"
-			;;
-		application/x-tar)
-			tar -tf "$file"
-			;;
-		application/x-bzip2)
-			tar -tjf "$file"
-			;;
-		application/gzip)
-			tar -tzf "$file"
-			;;
-		application/x-rar)
-			unrar l "$file"
-			;;
-		text/*)
-			highlight -O ansi -lVJ $3 "$file"
-			;;
-		image/*)
-			orientation="$(identify -format '%[EXIF:Orientation]\n' -- "$file")"
-			if [ -n "$orientation" ] && [ "$orientation" != 1 ]; then
-				cache="$(hash "$file").jpg"
-				cache "$cache" "$@"
-				convert -- "$file" -auto-orient "$cache"
-				draw "$cache" "$@"
-			else
-				draw "$file" "$@"
-			fi
-			;;
-		video/*)
+	application/pdf)
+		cache=$(hash "$file")
+		cache "$cache".jpg "$@"
+		pdftoppm -jpeg -f 1 -singlefile "$file" "$cache"
+		draw "$cache".jpg "$@"
+		;;
+	image/*)
+		orientation="$(identify -format '%[EXIF:Orientation]\n' -- "$file")"
+		if [ -n "$orientation" ] && [ "$orientation" != 1 ]
+		then
 			cache="$(hash "$file").jpg"
 			cache "$cache" "$@"
-			ffmpegthumbnailer -i "$file" -o "$cache" -s 0
+			convert -- "$file" -auto-orient "$cache"
 			draw "$cache" "$@"
-			;;
-		*)
-			file -Lb -- "$file" | fold -s -w $3
-			#ffmpeg -i "$file" -f pulse default
-			;;
+		else
+			draw "$file" "$@"
+		fi
+		;;
+	video/*)
+		cache="$(hash "$file").jpg"
+		cache "$cache" "$@"
+		ffmpegthumbnailer -i "$file" -o "$cache" -s 0
+		draw "$cache" "$@"
+		;;
+	*)
+		file -Lb -- "$file" | fold -s -w $3
+		#ffmpeg -i "$file" -f pulse default
+		;;
 	esac
 fi
 
